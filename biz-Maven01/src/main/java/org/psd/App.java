0945de57fd9b2aca81e561383e8b19627fe319bd
Jarcan <171005028@qq.com>
2024-11-07 11:15:30 +0800

    创建Maven项目并引用nacos

---
+++ biz-Maven01/src/main/java/org/psd/App.java
@@ -0,0 +1,14 @@
+package org.psd;
+
+/**
+ * Hello world!
+ *
+ * @author pengshidun
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
